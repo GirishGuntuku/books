22cc9aba9de04365d0e0e8fd89d3497b3fb5ee48
Girish Guntuku <S531369@nwmissouri.edu>
2019-02-08 14:26:04 -0600

    Added Unit tests

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -1,14 +1,52 @@
 using System;
 using Xunit;
 
+using System;
+using Xunit;
+
 namespace UnitTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Divide_WithTwoInts()
         {
-
+            var expected = 3;
+            var a = 6;
+            var b = 2;
+            var actual = Divide(a, b);
+            Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void Divide_WithZero()
+        {
+            var expected = 0;
+            var a = 0;
+            var b = 6;
+            var actual = Divide(a, b);
+            Assert.Equal(expected, actual);
+        }
+
+
+
+
+        [Theory]
+        [InlineData(1, 1, 1)]
+        [InlineData(-6, -2, 3)]
+        [InlineData(-8, 2, -4)]
+        public void Divide_WithMultipleInputs(int n1, int n2, int value)
+        {
+            Assert.Equal(Divide(n1, n2), value);
+        }
+
+
+
+        int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+
     }
 }
